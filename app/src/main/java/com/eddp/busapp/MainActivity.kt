57083adc0a916b0694201029ccae0bc19d4eb15b
Empire Démocratique du Poulpe <alexis.lecomte@supinfo.com>
2021-01-11 15:03:02 +0100

    Added the main view pager and the bottom navigation bar

--- app/src/main/java/com/eddp/busapp/MainActivity.kt
+++ app/src/main/java/com/eddp/busapp/MainActivity.kt
@@ -6,16 +6,23 @@ import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.eddp.busapp.data.Post
 import com.eddp.busapp.data.Station
 import com.eddp.busapp.data.StationAPI
 import com.eddp.busapp.data.StationResponse
 import com.eddp.busapp.interfaces.AsyncDataObservable
 import com.eddp.busapp.interfaces.AsyncDataObserver
+import com.eddp.busapp.views.ZoomOutPageTransformer
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Call
@@ -25,13 +32,18 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MainActivity : AppCompatActivity(), AsyncDataObservable {
-    private var _observers: MutableList<AsyncDataObserver?> = ArrayList()
-    private var _fragments: List<Fragment> = listOf(Home(), StationsViewPager(), UserPics())
+    private val _observers: MutableList<AsyncDataObserver?> = ArrayList()
+
+    private lateinit var _viewPager: ViewPager2
+    private lateinit var _viewPagerAdapter: MainViewPagerAdapter
+    private lateinit var _bottomNavBar: BottomNavBar
 
     private var _posts: List<Post>? = null
     private var _stations: List<Station>? = null
 
     // Getters
+    fun getViewPager() : ViewPager2 = this._viewPager
+
     fun getPosts() : List<Post>? = this._posts
     fun getStations() : List<Station>? = this._stations
 
@@ -39,9 +51,28 @@ class MainActivity : AppCompatActivity(), AsyncDataObservable {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Init view
+        this._bottomNavBar = BottomNavBar(this)
+
+        this._viewPager = findViewById(R.id.main_view_pager)
+        this._viewPagerAdapter = MainViewPagerAdapter(this)
+        this._viewPager.adapter = this._viewPagerAdapter
+        this._viewPager.registerOnPageChangeCallback(MainViewPagerCallback(this._bottomNavBar))
+        this._viewPager.setPageTransformer(ZoomOutPageTransformer())
+
+        // Get data
         getStationsFromAPI()
     }
 
+    override fun onBackPressed() {
+        if (this._viewPager.currentItem == 0) {
+            super.onBackPressed()
+        } else {
+            this._viewPager.currentItem = this._viewPager.currentItem - 1
+        }
+    }
+
+    // Async data
     private fun getStationsFromAPI() {
         val moshi = Moshi.Builder()
                 .add(KotlinJsonAdapterFactory())
@@ -145,5 +176,71 @@ class MainActivity : AppCompatActivity(), AsyncDataObservable {
                 .setMessage(message)
                 .setPositiveButton(positiveButtonText, onPositiveButtonClick)
         }
+    }
+}
+
+// Navigation
+class MainViewPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
+    private val _fragments: List<Fragment> = listOf(
+            Home(),
+            StationsViewPager(),
+            Settings()
+    )
+
+    fun getFragmentById(position: Int) : Fragment = this._fragments[position]
+
+    override fun getItemCount() : Int = this._fragments.size
+
+    override fun createFragment(position: Int) : Fragment = this._fragments[position]
+}
+
+class MainViewPagerCallback(bottomNavBar: BottomNavBar)
+    : ViewPager2.OnPageChangeCallback() {
+    private val _bottomNavBar = bottomNavBar
+
+    override fun onPageSelected(position: Int) {
+        super.onPageSelected(position)
+        _bottomNavBar.setSelectedItem(position)
+    }
+}
+
+class BottomNavBar(activity: MainActivity) : BottomNavigationView.OnNavigationItemSelectedListener {
+    private val _activity = activity
+    private val _bottomNavBar: BottomNavigationView = this._activity.findViewById(R.id.bottom_nav_bar)
+
+    private val _menu: Menu = this._bottomNavBar.menu
+    private val _items: MutableList<MenuItem> = ArrayList()
+    private var _previousSelectedMenu: Int = 0
+
+    init {
+        for (i in 0 until this._menu.size())  {
+            this._items.add(this._menu.getItem(i))
+        }
+
+        // Add events
+        this._bottomNavBar.setOnNavigationItemSelectedListener(this)
+    }
+
+    // Setters
+    fun setSelectedItem(position: Int) {
+        updatePreviousSelectedItem()
+        this._items[position].isChecked = true
+    }
+
+    private fun updatePreviousSelectedItem() {
+        this._previousSelectedMenu = this._bottomNavBar.selectedItemId
+    }
+
+    // Navigation
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Get current item pos
+        val itemPos = this._items.indexOf(item)
+        if (this._previousSelectedMenu == itemPos) return false
+
+        // Update
+        this._activity.getViewPager().setCurrentItem(itemPos, true)
+        updatePreviousSelectedItem()
+
+        return true
     }
 }
