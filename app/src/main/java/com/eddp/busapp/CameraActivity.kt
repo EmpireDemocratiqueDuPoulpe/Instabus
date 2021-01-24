78491939f24b01bdc2cfc73d244b762b596aa6ee
Breadator258 <maxence.pawlowski@supinfo.com>
2021-01-24 17:37:21 +0100

    Add toolbar to camera activity and changed his color

--- app/src/main/java/com/eddp/busapp/CameraActivity.kt
+++ app/src/main/java/com/eddp/busapp/CameraActivity.kt
@@ -4,11 +4,14 @@ import android.Manifest
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.widget.Toolbar
 
 class CameraActivity : AppCompatActivity() {
     private var _stationId: Long = Long.MIN_VALUE
+    private lateinit var _toolbar: Toolbar
 
     private lateinit var _requestPermissionLauncher: ActivityResultLauncher<Array<String>>
 
@@ -26,6 +29,12 @@ class CameraActivity : AppCompatActivity() {
         if (id != null) {
             this._stationId = id
         }
+
+        this._toolbar = findViewById(R.id.toolbar)
+        this._toolbar.title = getString(R.string.cam_actionbar_title)
+        setSupportActionBar(this._toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true);
+        supportActionBar?.setDisplayShowHomeEnabled(true);
 
         // Ask for permissions
         this._requestPermissionLauncher =
@@ -52,6 +61,11 @@ class CameraActivity : AppCompatActivity() {
         if (missingPermissions != null) {
             this._requestPermissionLauncher.launch(missingPermissions.toTypedArray())
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 
     companion object {
