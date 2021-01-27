b7c497fe66cc6282b9acd14029671cfa07915397
Empire Démocratique du Poulpe <alexis.lecomte@supinfo.com>
2021-01-27 22:09:20 +0100

    Login and register refactor

--- app/src/main/java/com/eddp/busapp/AuthActivity.kt
+++ app/src/main/java/com/eddp/busapp/AuthActivity.kt
@@ -6,21 +6,21 @@ import android.util.Patterns
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
-
 class AuthActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
-    }
-
-    fun checkEmail(text: EditText?): Boolean {
-        val email: CharSequence = text!!.text.toString()
+    // Getters
+    fun isEmailValid(field: EditText?) : Boolean {
+        val email: CharSequence = field?.text.toString()
         return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
 
-    fun isEmpty(text: EditText?): Boolean {
-        val str: CharSequence = text!!.text.toString()
-        return TextUtils.isEmpty(str)
+    fun isEmpty(field: EditText?) : Boolean {
+        val value: CharSequence = field?.text.toString()
+        return TextUtils.isEmpty(value)
+    }
+
+    // Views
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
     }
 }
--- app/src/main/java/com/eddp/busapp/Register.kt
+++ app/src/main/java/com/eddp/busapp/Register.kt
@@ -7,23 +7,26 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.Navigation
 import com.eddp.busapp.data.WebServiceLink
 import com.eddp.busapp.interfaces.WebServiceReceiver
 
 class Register : Fragment(), WebServiceReceiver {
-    private lateinit var _username: EditText
-    private lateinit var _email: EditText
-    private lateinit var _password: EditText
-    private lateinit var _confirmPassword: EditText
-    private lateinit var _register: Button
-    private lateinit var _login_link: TextView
     private var _activity: AuthActivity? = null
+
     private lateinit var _navController: NavController
-    private var _webServiceLink: WebServiceLink? = null
+
+    private lateinit var _errorText: TextView
+    private lateinit var _usernameField: EditText
+    private lateinit var _emailField: EditText
+    private lateinit var _passwordField: EditText
+    private lateinit var _confirmPasswordField: EditText
+    private lateinit var _registerBtn: Button
+    private lateinit var _loginLink: TextView
+
+    private var _webServiceLink = WebServiceLink(this)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,75 +38,119 @@ class Register : Fragment(), WebServiceReceiver {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // Get activity
         if(activity is AuthActivity){
-            _activity = (activity as AuthActivity)
+            this._activity = (activity as AuthActivity)
         }
-        val navHostFragment = _activity?.supportFragmentManager
-            ?.findFragmentById(R.id.auth_fragment_container) as NavHostFragment
-        this._webServiceLink = WebServiceLink(this)
-        _navController = navHostFragment.navController
+
+        // Views
         initFields(view)
+
+        // Navigation
+        this._navController = Navigation.findNavController(view)
     }
 
     private fun initFields(view: View) {
-        _username = view.findViewById(R.id.register_username)
-        _password = view.findViewById(R.id.register_password)
-        _confirmPassword = view.findViewById(R.id.register_confirm_password)
-        _email = view.findViewById(R.id.register_email)
-        _login_link = view.findViewById(R.id.login_link)
-        _register = view.findViewById(R.id.register_button)
-        _register.setOnClickListener { checkData() }
-        _login_link.setOnClickListener { _navController.navigate(R.id.action_register_to_login) }
+        this._errorText = view.findViewById(R.id.register_errors)
+        this._usernameField = view.findViewById(R.id.register_username)
+        this._emailField = view.findViewById(R.id.register_email)
+        this._passwordField = view.findViewById(R.id.register_password)
+        this._confirmPasswordField = view.findViewById(R.id.register_confirm_password)
+
+        this._registerBtn = view.findViewById(R.id.register_button)
+        this._registerBtn.setOnClickListener { register() }
+
+        this._loginLink = view.findViewById(R.id.login_link)
+        this._loginLink.setOnClickListener { goToLoginPage() }
     }
 
-    private fun checkData() {
-        var validatedCheck = true
-        if (_activity?.isEmpty(_username) == true) {
-            val t =
-                Toast.makeText(_activity, "Username is required.", Toast.LENGTH_SHORT)
-            t.show()
-            validatedCheck = false
-        }
-        if (_username.text.length > 32) {
-            val t =
-                Toast.makeText(_activity, "Username must not exceed 32 characters.", Toast.LENGTH_LONG)
-            t.show()
-            validatedCheck = false
-        }
-        if (_activity?.isEmpty(_password) == true) {
-            _password.error = "Password is required."
-            validatedCheck = false
-        }
-        if (_activity?.isEmpty(_confirmPassword) == true) {
-            _confirmPassword.error = "Confirm password is required."
-            validatedCheck = false
-        }
-        if(_password.text !== _confirmPassword.text) {
-            _confirmPassword.error = "Password and password confirmation doesn't match."
-            validatedCheck = false
-        }
-        if (_activity?.checkEmail(_email) == false) {
-            _email.error = "Email is required."
-            validatedCheck = false
-        }
+    // Register
+    private fun register() {
+        removeError()
 
-        if(validatedCheck){
-            this._webServiceLink?.addUser(
-                _username.text.toString(),
-                _email.text.toString(),
-                _password.text.toString()
+        if (this._activity == null)
+            return showError("Fatal error. Update the app and try again.")
+
+        if (checkFields()) {
+            this._webServiceLink.addUser(
+                this._usernameField.text.toString(),
+                this._emailField.text.toString(),
+                this._passwordField.text.toString()
             )
         }
     }
 
+    private fun checkFields() : Boolean {
+        var completed = true
+
+        // Username
+        if (this._activity!!.isEmpty(this._usernameField)) {
+            showError(this._activity!!.getString(R.string.auth_err_empty_username), this._usernameField)
+            completed = false
+        } else if (this._usernameField.text.length > 32) {
+            showError(this._activity!!.getString(R.string.auth_err_too_long_username), this._usernameField)
+            completed = false
+        }
+
+        // Email
+        if (!this._activity!!.isEmailValid(this._emailField)) {
+            showError(this._activity!!.getString(R.string.auth_err_empty_email), this._emailField)
+            completed = false
+        }
+
+        // Passwords
+        if (this._activity!!.isEmpty(this._passwordField)) {
+            showError(this._activity!!.getString(R.string.auth_err_empty_password), this._passwordField)
+            completed = false
+        }
+
+        if (this._activity!!.isEmpty(this._confirmPasswordField)) {
+            showError(this._activity!!.getString(R.string.auth_err_empty_password), this._confirmPasswordField)
+            completed = false
+        }
+
+        if(this._passwordField.text.toString() != this._confirmPasswordField.text.toString()) {
+            showError(
+                this._activity!!.getString(R.string.auth_err_password_match),
+                this._passwordField,
+                this._confirmPasswordField
+            )
+            completed = false
+        }
+
+        return completed
+    }
+
+    private fun showError(message: String, vararg fields: EditText) {
+        if (this._errorText.text.isEmpty()) {
+            this._errorText.text = message
+        }
+
+        for (field in fields) {
+            field.error = message
+        }
+    }
+
+    private fun removeError() {
+        this._errorText.text = ""
+    }
+
+    // Navigation
+    private fun goToLoginPage() {
+        this._navController.navigate(R.id.action_register_to_login)
+    }
+
+    private fun goToMainActivity() {}
+
+    // Web service
     override fun addSuccessful(success: Boolean) {
         super.addSuccessful(success)
-        if(success){
-            _navController.navigate(R.id.action_register_to_login)
-        }else{
-            val t =
-                Toast.makeText(_activity, "Error when creating new account.", Toast.LENGTH_LONG)
-            t.show()
+
+        if(success) {
+            goToMainActivity()
+        } else {
+            showError(this._activity!!.getString(R.string.auth_err_unknown_register))
         }
     }
 }
